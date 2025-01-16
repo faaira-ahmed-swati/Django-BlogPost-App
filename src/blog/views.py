330c918c4bfe568e42e4ab25cf64ed12487c8aa3
Faaira Swati <faaira.swati@auxcube.com>
2025-01-17 00:58:01 +0500

    feat: update and delete blogposts

--- src/blog/views.py
+++ src/blog/views.py
@@ -1,7 +1,10 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
 from django.http import Http404
+from django.contrib.auth.decorators import login_required
+from django.contrib.admin.views.decorators import staff_member_required
 # Create your views here.
 from .models import BlogPost
+from .forms import BlogpostModelForm
 
 
 # def blog_post_detail_page_by_id(request, id):
@@ -28,9 +31,19 @@ def blog_post_list_view(request):
     return render(request, template_name, context)
 
 
+@staff_member_required
+# @login_required
 def blog_post_create_view(request):
-    template_name = 'blog/create.html'
-    context = {'title': 'Blog Posts Create', 'form': None}
+    form = BlogpostModelForm(request.POST or None)
+    if form.is_valid():
+        obj = form.save(commit=False)
+        obj.user = request.user
+        obj.save()
+        # can do above instead of doing .create
+        # obj = BlogPost.objects.create(**form.cleaned_data)
+        form = BlogpostModelForm()
+    template_name = 'form.html'
+    context = {'title': 'Blog Posts Create', 'form': form}
     return render(request, template_name, context)
 
 # Retrieves only one document
@@ -43,15 +56,23 @@ def blog_post_detail_view(request, slug):
     return render(request, template_name, context)
 
 
+@staff_member_required
 def blog_post_update_view(request, slug):
     obj = get_object_or_404(BlogPost, slug=slug)
-    template_name = 'blog/update.html'
-    context = {'title': 'Blog Posts Update', "object": obj, 'form': None}
+    form = BlogpostModelForm(request.POST or None, instance=obj)
+    if form.is_valid():
+        form.save()
+    template_name = 'form.html'
+    context = {'title': f'Update {obj.title}', 'form': form}
     return render(request, template_name, context)
 
 
+@staff_member_required
 def blog_post_delete_view(request, slug):
     obj = get_object_or_404(BlogPost, slug=slug)
     template_name = 'blog/delete.html'
+    if request.method == 'POST':
+        obj.delete()
+        return redirect('/blog')
     context = {'title': 'Blog Posts Delete', "object": obj}
     return render(request, template_name, context)
--- src/try_django/views.py
+++ src/try_django/views.py
@@ -3,6 +3,8 @@ from django.http import HttpResponse
 from django.shortcuts import render
 from django.template.loader import get_template
 
+from .forms import ContactForm
+
 
 def home_page(request):
     # Render context in templates
@@ -14,7 +16,15 @@ def about_page(request):
 
 
 def contact_page(request):
-    return render(request, 'hello_world.html', {"title": "Contact Us"})
+    form = ContactForm(request.POST or None)
+    if form.is_valid():
+        print(form.cleaned_data)
+        form = ContactForm()
+    context = {
+        "title": "Contact Us",
+        "form": form
+    }
+    return render(request, 'form.html', context)
 
 
 def test_page(request):
